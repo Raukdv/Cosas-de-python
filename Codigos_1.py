44f5ae0f1bf19b79225c0a0658322241b890b579
Raul Diaz <48073748+Raukdv@users.noreply.github.com>
2021-04-01 19:58:15 -0400

    Update Codigos_1.py

--- Codigos_1.py
+++ Codigos_1.py
@@ -77,3 +77,19 @@ message = car.items() ^ car2.items()
 keys_changed =  [x for x,y in message if x]
 
 print(keys_changed)
+
+###################################
+#Url checker
+import re
+regex = re.compile(
+         r'^(?:http|ftp)s?://' # http:// or https://
+         r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
+         r'localhost|' #localhost...
+         r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
+         r'(?::\d+)?' # optional port
+         r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+a = (re.match(regex, "http://www.example.com/") is not None) # True
+b = (re.match(regex, "example.com") is not None) #False
+print(a)
+print(b)
+########################################
